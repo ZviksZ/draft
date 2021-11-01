d32193359628ce668a3f78ec3afefba4021c5bab
Nikita Kapshtyk <viks2332@gmail.com>
2021-11-01 16:34:09 +0300

    custom link

--- src/components/atoms/AppLink.tsx
+++ src/components/atoms/AppLink.tsx
@@ -2,13 +2,28 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { LinkProps } from "next/dist/client/link";
+import classNames from "classnames";
 
-export function AppLink(props: React.PropsWithChildren<LinkProps>) {
+interface AppLinkProps extends React.PropsWithChildren<LinkProps> {
+  className?: string;
+  activeClassName?: string;
+  testId?: string;
+}
+
+export function AppLink({
+  className = "",
+  activeClassName = "tw-text-blue-300",
+  testId = "",
+  ...props
+}: AppLinkProps) {
   const router = useRouter();
-
+  const isActive = router.pathname === props.href;
   return (
     <Link {...props}>
-      <a className={router.pathname == props.href ? "tw-text-blue-300" : "tw-text-black"}>
+      <a
+        data-testId={testId}
+        className={classNames("tw-text-black", className, isActive && activeClassName)}
+      >
         {props.children}
       </a>
     </Link>
--- src/components/common/Sidebar/Sidebar.tsx
+++ src/components/common/Sidebar/Sidebar.tsx
@@ -5,12 +5,15 @@ interface SidebarProps {}
 
 export function Sidebar({}: SidebarProps) {
   return (
-    <div className="tw-flex tw-items-start tw-flex-col tw-w-48 tw-h-full tw-shadow-inner tw-bg-gray-300">
-      <div className="tw-p-4 tw-text-2xl tw-text-black tw-uppercase tw-font-extrabold">Draft</div>
-
+    <div className="tw-flex tw-items-start tw-flex-col tw-w-48 tw-h-full tw-shadow-inner tw-bg-gray-900">
+      <div className="tw-p-4 tw-text-2xl tw-text-white tw-uppercase tw-font-extrabold">Draft</div>
       <div className="tw-flex tw-flex-col tw-items-start tw-space-y-3">
-        <AppLink href="/">Home</AppLink>
-        <AppLink href="/projects">Projects</AppLink>
+        <AppLink href="/" className="tw-text-white">
+          Home
+        </AppLink>
+        <AppLink href="/projects" className="tw-text-white">
+          Projects
+        </AppLink>
       </div>
     </div>
   );
